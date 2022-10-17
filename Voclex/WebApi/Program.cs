ff256289c206b897f8bb965524845bf397aa63b2
helgezec <115068202+helgezec@users.noreply.github.com>
2022-10-17 09:37:08 +0300

    enum serialization for APIs enabled

--- Voclex/WebApi/Program.cs
+++ Voclex/WebApi/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using Application.DataAccess;
 using Application.Models;
 using Application.Services;
@@ -11,7 +12,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
