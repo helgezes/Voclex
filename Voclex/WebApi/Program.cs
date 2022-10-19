980666c97d0c1e6b77c9613019c0028de8aa1436
helgezec <115068202+helgezec@users.noreply.github.com>
2022-10-19 16:58:50 +0300

    add localhost address of blazor app to WebApi cors add httpclient with address of webapi container

--- Voclex/WebApi/Program.cs
+++ Voclex/WebApi/Program.cs
@@ -11,6 +11,7 @@ using Shared;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options => options.AddDefaultPolicy(c => c.WithOrigins("https://localhost:7181")));
 
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
@@ -39,6 +40,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
