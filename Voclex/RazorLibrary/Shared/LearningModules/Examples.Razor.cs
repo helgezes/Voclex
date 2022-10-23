9eaf978cfb9fb64d7c2c9d57730ba214bd1ef070
helgezec <115068202+helgezec@users.noreply.github.com>
2022-10-23 11:04:28 +0300

    examples page added

---
+++ Voclex/RazorLibrary/Shared/LearningModules/Examples.Razor.cs
@@ -0,0 +1,14 @@
+﻿using Shared.DataTransferObjects;
+
+namespace RazorLibrary.Shared.LearningModules
+{
+    public partial class Examples : LearningModule<ExampleDto>
+    {
+        protected override string ApiPath => "Examples/GetList";
+
+        protected override Task OnParametersSetAsync()
+        {
+            return base.OnParametersSetAsync();
+        }
+    }
+}
