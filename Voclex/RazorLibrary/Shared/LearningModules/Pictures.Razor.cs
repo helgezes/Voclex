352e14b85216a74a8aa54c6c4df1aa9eef1d32f5
helgezec <115068202+helgezec@users.noreply.github.com>
2022-10-25 08:31:25 +0300

    Pictures page added

---
+++ Voclex/RazorLibrary/Shared/LearningModules/Pictures.Razor.cs
@@ -0,0 +1,17 @@
+﻿using Shared.DataTransferObjects;
+
+namespace RazorLibrary.Shared.LearningModules
+{
+    public partial class Pictures : LearningModule<PictureDto>
+    {
+        private string pictureHost;
+
+        protected override string ApiPath => "Pictures/GetList";
+
+        protected override Task OnInitializedAsync()
+        {
+            pictureHost = Http.BaseAddress.AbsoluteUri;
+            return base.OnInitializedAsync();
+        }
+    }
+}
