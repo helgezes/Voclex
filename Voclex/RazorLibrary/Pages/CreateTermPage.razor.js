362c15cdad7c41a6016a6d236b52ef0d13e23177
helgezec <115068202+helgezec@users.noreply.github.com>
2022-11-13 16:35:09 +0300

    Succesful term creation modal added

---
+++ Voclex/RazorLibrary/Pages/CreateTermPage.razor.js
@@ -0,0 +1,10 @@
+﻿const successModal = new bootstrap.Modal('#successfulCreation');
+
+export function ShowSuccessfulCreationModal() {
+    successModal.show();
+}
+
+export function DisposeModalAndGoToUrl(path) {
+    successModal.dispose();
+    location.href = location.origin + path;
+}
