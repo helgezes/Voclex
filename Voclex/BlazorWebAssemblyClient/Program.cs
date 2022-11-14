7b9b90a31469da42e521716b10ed8df846c1e34c
helgezec <115068202+helgezec@users.noreply.github.com>
2022-11-14 10:39:25 +0300

    web api port changed

--- Voclex/BlazorWebAssemblyClient/Program.cs
+++ Voclex/BlazorWebAssemblyClient/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:49167/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44392/") });
 
 await builder.Build().RunAsync();
