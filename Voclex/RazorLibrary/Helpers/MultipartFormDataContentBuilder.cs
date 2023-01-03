ea6e6077162056e9898b89397bb176988cfd657c
helgezes <120435391+helgezes@users.noreply.github.com>
2023-01-03 05:11:28 +0300

    MultiPardFormDataContentBuilder added

---
+++ Voclex/RazorLibrary/Helpers/MultipartFormDataContentBuilder.cs
@@ -0,0 +1,48 @@
+﻿using SharedLibrary.DataTransferObjects;
+using System.Reflection;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Http;
+
+namespace RazorLibrary.Helpers
+{
+	public sealed  class MultipartFormDataContentBuilder
+	{
+		private MultipartFormDataContent content = new();
+
+		public MultipartFormDataContentBuilder AddObjectPublicProperties(object obj)
+		{
+			foreach (var property in obj.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+			{
+				var propertyValue = property.GetValue(obj)?.ToString();
+				if (propertyValue == null) continue;
+
+				content.Add(new StringContent(propertyValue), property.Name);
+			}
+
+			return this;
+		}
+
+		public MultipartFormDataContentBuilder AddFile(IBrowserFile file, int maxAllowedFileSizeInMb = 5, string nameInContent = "file")
+		{
+			var fileContent = new StreamContent(file.OpenReadStream(maxAllowedFileSizeInMb * 1024 * 1024));
+			fileContent.Headers.Add("Content-Type", "application/octet-stream");
+			content.Add(fileContent, nameInContent, file.Name);
+
+			return this;
+		}
+
+		public MultipartFormDataContent Build()
+		{
+			var result = content;
+
+			Reset();
+
+			return result;
+		}
+
+		public void Reset()
+		{
+			content = new MultipartFormDataContent();
+		}
+	}
+}
