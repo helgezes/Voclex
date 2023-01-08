e1bb94eaf603d95a8d324eda4f845690f324760d
helgezes <120435391+helgezes@users.noreply.github.com>
2023-01-08 09:48:17 +0300

    CreateTermPage and EditTermPage code moved to .cs files

---
+++ Voclex/RazorLibrary/Pages/CreateTermPage.razor.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using RazorLibrary.Shared.CreateLearningModules;
+using SharedLibrary.DataTransferObjects;
+using System.Net.Http.Json;
+
+namespace RazorLibrary.Pages
+{
+    public partial class CreateTermPage : ComponentBase
+    {
+        [Parameter]
+        public int DictionaryId { get; set; }
+
+        private readonly Type[] modules = new[] { typeof(CreateDefinition), typeof(CreateExamples), typeof(CreatePicture) };
+
+        private TermDto newTerm = null!;
+        private int newTermId = 0;
+        private bool saveEnabled;
+
+        private DynamicComponent[] editableLearningModules;
+        private IJSObjectReference jsModule;
+
+        private string GoBackToDictionaryUrl => $"/dictionary/{DictionaryId}";
+        private string ContinueEditingUrl => $"/term/edit/{newTermId}";
+
+        protected override void OnInitialized()
+        {
+            editableLearningModules = new DynamicComponent[modules.Length];
+            newTerm = new(0, string.Empty, DictionaryId);
+            saveEnabled = true;
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (!firstRender) return;
+
+            jsModule = await JS.InvokeAsync<IJSObjectReference>(
+                "import", $"./_content/{nameof(RazorLibrary)}/Pages/{nameof(CreateTermPage)}.razor.js");
+
+        }
+
+        private async Task SaveChanges()
+        {
+            saveEnabled = false;
+
+            var response = await Http.PostAsJsonAsync("Terms", newTerm);
+            newTermId = int.Parse(await response.Content.ReadAsStringAsync());
+
+            var modulesSaveChangesTasks = editableLearningModules
+                .Select(m => ((ICreatableLearningModule)m.Instance).SaveChanges(newTermId))
+                .ToArray();
+
+            await Task.WhenAll(modulesSaveChangesTasks);
+
+            await jsModule.InvokeVoidAsync("ShowSuccessfulCreationModal");
+        }
+
+        private async Task DisposeModalAndGoToUrl(string path)
+        {
+            await jsModule.InvokeVoidAsync("DisposeModalAndGoToUrl", path);
+        }
+    }
+}
---
+++ Voclex/RazorLibrary/Pages/EditTermPage.razor.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Components;
+using RazorLibrary.Shared.CreateLearningModules;
+using RazorLibrary.Shared.EditLearningModules;
+using RazorLibrary.Shared;
+using SharedLibrary.DataTransferObjects;
+using System.Net.Http.Json;
+
+namespace RazorLibrary.Pages
+{
+    public partial class EditTermPage : ComponentBase
+    {
+        [Parameter]
+        public int Id { get; set; }
+
+        private TermDto currentTerm = new(0, string.Empty, 1); //todo dictionary id;
+
+        private readonly Dictionary<Type, Type> editToCreateModules =
+            new()
+            {
+            { typeof(EditDefinition), typeof(CreateDefinition) },
+            { typeof(EditExamples), typeof(CreateExamples) },
+            { typeof(EditPicture), typeof(CreatePicture) }
+            };
+        private readonly List<Type> createModules = new();
+        private Type[] editModules;
+
+        private DynamicComponent[] editableLearningModules;
+        private readonly Dictionary<Type, DynamicComponent?> creatableLearningModulesDictionary = new();
+        private SuccessAlert? successAlert;
+
+        private bool saveEnabled = false;
+        private int learningModulesInitializedCount = 0;
+
+        protected override async Task OnInitializedAsync()
+        {   //todo mb form as a component, add and edit page separate, using this component
+            editModules = editToCreateModules.Keys.ToArray();
+            editableLearningModules = new DynamicComponent[editToCreateModules.Count];
+
+            TermDto? loadedTerm;
+            if (Id != default && (loadedTerm = await Http.GetFromJsonAsync<TermDto>($"Terms?id={Id}")) != null)
+                currentTerm = loadedTerm;
+
+        }
+
+        private async Task SaveChanges()
+        {
+            saveEnabled = false;
+
+            await SendChangesToServer();
+
+            saveEnabled = true;
+
+            successAlert!.Show(5000);
+        }
+
+        private async Task SendChangesToServer()
+        {
+            var responseTask = Http.PutAsJsonAsync("Terms", currentTerm);
+
+            var creatableModulesSaveResultTasks =  //todo refactor
+                creatableLearningModulesDictionary.Values.Where(m => m != null)
+                .Select(m =>
+                {
+                    var moduleInstance = (ICreatableLearningModule)m.Instance;
+                    var saveChangesTask = moduleInstance.SaveChanges(currentTerm.Id);
+                    return new { moduleInstance, saveChangesTask };
+                }).ToArray();
+
+            var modulesSaveChangesTasks = editableLearningModules
+                .Select(m => ((IEditableLearningModule)m.Instance).SaveChanges())
+                .Append(responseTask)
+                .Concat(creatableModulesSaveResultTasks.Select(x => x.saveChangesTask))
+                .ToArray();
+
+            await Task.WhenAll(modulesSaveChangesTasks);
+
+            foreach (var savingResult in creatableModulesSaveResultTasks)
+            {
+                if (!await savingResult.saveChangesTask) continue;
+                var createdModule = savingResult.moduleInstance;
+
+                LoadCreatedModule(createdModule);
+            }
+        }
+
+        private void LoadCreatedModule(ICreatableLearningModule createdModule)
+        {
+            var editModule = editToCreateModules.First(e => e.Value == createdModule.GetType()).Key;
+            var editComponent = editableLearningModules.First(m => m.Instance.GetType() == editModule);
+            ((IEditableLearningModule)editComponent.Instance).LoadEntities();
+        }
+
+        private void OnLearningModuleInitialized(OnInitializationEventArgs eventArgs)
+        {
+            AddCreateModuleIfEntityIsNotLoaded(eventArgs);
+
+            learningModulesInitializedCount++;
+            if (learningModulesInitializedCount < editModules.Length) return;
+
+            saveEnabled = true;
+        }
+
+        private void AddCreateModuleIfEntityIsNotLoaded(OnInitializationEventArgs eventArgs)
+        {
+            var createModule = editToCreateModules[eventArgs.Type];
+            if (eventArgs.IsEntityLoaded)
+            {
+                createModules.Remove(createModule);
+                creatableLearningModulesDictionary.Remove(createModule);
+                return;
+            }
+
+            if (createModules.Contains(createModule)) return;
+
+            createModules.Add(createModule);
+        }
+
+        protected IDictionary<string, object> GetLearningModuleParameters()
+        {
+            return new Dictionary<string, object>
+        {
+            { "TermId", Id },
+            { "OnInitializationComplete",
+                EventCallback.Factory.Create<OnInitializationEventArgs>(this, OnLearningModuleInitialized) }
+        };
+        }
+    }
+}
