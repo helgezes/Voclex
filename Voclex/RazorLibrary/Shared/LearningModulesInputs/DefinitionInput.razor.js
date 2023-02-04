27fb0ce67620d1ee4be9f966ff0c4029deddb930
helgezes <120435391+helgezes@users.noreply.github.com>
2023-02-04 16:13:48 +0300

    definition input textarea adjust height

---
+++ Voclex/RazorLibrary/Shared/LearningModulesInputs/DefinitionInput.razor.js
@@ -0,0 +1,6 @@
+﻿const textarea = document.getElementById("definition-value");
+textarea.addEventListener("input", AdjustInputHeight);
+
+export function AdjustInputHeight() {
+    textarea.style.height = textarea.scrollHeight + "px";
+}
