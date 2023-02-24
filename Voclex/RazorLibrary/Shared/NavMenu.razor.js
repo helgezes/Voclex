ef9b9c1e6a50f1bfc437d545e6dea54a5a9076b8
helgezes <120435391+helgezes@users.noreply.github.com>
2023-02-24 17:48:48 +0300

    logout button added

---
+++ Voclex/RazorLibrary/Shared/NavMenu.razor.js
@@ -0,0 +1,23 @@
+﻿export function InitializeLogoutPopover(){
+    bootstrap.Popover.Default.allowList.button = [];
+    const logoutPopoverTrigger = document.querySelector('#logout_popover');
+
+    if (logoutPopoverTrigger === null) return;
+
+    new bootstrap.Popover(logoutPopoverTrigger, {
+        placement: 'bottom',
+        html: true,
+        content: '<button class="btn btn-danger" id="logout">Log out</button>'
+    });
+
+    document.addEventListener("shown.bs.popover", (event) => {
+        if (event.srcElement.id !== "logout_popover") return;
+
+        document.querySelector("#logout").addEventListener("click", logOut);
+    });
+}
+
+function logOut() {
+    localStorage.removeItem("token");
+    window.location.reload();
+}
