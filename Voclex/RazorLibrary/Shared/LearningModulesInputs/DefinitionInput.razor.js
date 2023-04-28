0747043c3fc34007654958b904f3d6779fe9eeb4
helgezes <120435391+helgezes@users.noreply.github.com>
2023-04-28 12:05:05 +0800

    examples input adjustable height

--- Voclex/RazorLibrary/Shared/LearningModulesInputs/DefinitionInput.razor.js
+++
@@ -1,6 +0,0 @@
-﻿const textarea = document.getElementById("definition-value");
-textarea.addEventListener("input", AdjustInputHeight);
-
-export function AdjustInputHeight() {
-    textarea.style.height = textarea.scrollHeight + "px";
-}
---
+++ Voclex/RazorLibrary/Shared/LearningModulesInputs/Suggestions/StringSuggestionsComponent.razor.js
@@ -0,0 +1,14 @@
+﻿const parentElement = document.body;
+
+parentElement.addEventListener("input", (event) => {
+    if (event.target.classList.contains("adjustable-height")) {
+        AdjustAllInputHeights();
+    }
+});
+
+export function AdjustAllInputHeights() {
+    const textareas = document.querySelectorAll(".adjustable-height");
+    textareas.forEach((textarea) => {
+        textarea.style.height = textarea.scrollHeight + "px";
+    });
+}
