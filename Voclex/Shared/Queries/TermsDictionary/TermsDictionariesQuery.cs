9985ba9fc025feeeb7f2b4ee4a966095f7e432d2
helgezes <120435391+helgezes@users.noreply.github.com>
2023-05-13 20:58:46 +0800

    My Dictionaries page displaying only dictionaries for the current user

--- Voclex/Shared/Queries/TermsDictionary/TermsDictionariesQuery.cs
+++ Voclex/Shared/Queries/TermsDictionary/TermsDictionariesQuery.cs
@@ -1,14 +1,25 @@
 ﻿using System.Linq.Expressions;
 using Application.DataAccess;
+using Application.Models;
 using Application.Services;
+using SharedLibrary.Attributes;
 
 namespace SharedLibrary.Queries.TermsDictionary
 {
 	public class TermsDictionariesQuery : IQuery<Application.Models.TermsDictionary>
-	{
+    {
+        [UserId]
+        public int UserId { get; init; }
+
         public Expression<Func<Application.Models.TermsDictionary, bool>> WhereQuery(IDbContext context)
         {
-            return (_) => true;
+            var userIsAdmin = context.Users.Any(u => u.Id == UserId && u.Role == Role.Admin);
+            if (userIsAdmin)
+            {
+                return (dictionary) => dictionary.UserId == null;
+            }
+
+            return (dictionary) => dictionary.UserId == UserId;
         }
     }
 }
